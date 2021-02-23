b99a1a07a16207d1ba6749d794d5bb2f77479f58
b3lzebuth <leandrofelipecunha@hotmail.com>
2021-02-23 20:28:30 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+
+class App extends Component {
+
+criarNota(titulo, texto){
+  console.log(`nota criada ` + titulo + " " + texto);
+}
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota} />
+        <ListaDeNotas />
+      </section>
+    );
+  }
+}
+
+export default App;
